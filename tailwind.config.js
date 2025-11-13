7ffc0029cd9ae0d95168fdb90520a11ea6993389
Kamlesh Barwar <86289830+Kamlesh-Barwar@users.noreply.github.com>
2025-11-13 13:38:16 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*"],
+  content: [ "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {fontFamily: {
       mullish: ["Mulish", "sans-serif"],
+       plugins: [],
     },
     colors: {
       deepBlue: "#02042a",
